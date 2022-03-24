7eabf67adb5c2a011b7e5398c1c6dc8a74d78705
BrendaWyctoria <brendawyctoria29@gmail.com>
2022-03-23 23:04:00 -0300

    Commit inicial projeto fronted

--- backend/src/main/java/com/lista/demo/resources/ListaResource.java
+++ backend/src/main/java/com/lista/demo/resources/ListaResource.java
@@ -1,15 +1,14 @@
 package com.lista.demo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lista.demo.repository.ListaRepository;
-
-
-
 import com.lista.demo.models.Lista;
+import com.lista.demo.repository.ListaRepository;
 
 @RestController 
 @RequestMapping(value="/api")
